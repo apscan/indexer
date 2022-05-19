420961fc45fe95275fe1030b746bf987f94b5594
Bo Wu <bo@aptoslabs.com>
2022-05-18 20:20:45 -0700

    work in progress

--- ecosystem/indexer/api/types/src/mime_types.rs
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Aptos
-// SPDX-License-Identifier: Apache-2.0
-
-pub const BCS_SIGNED_TRANSACTION: &str = "application/x.aptos.signed_transaction+bcs";
-pub const JSON: &str = "application/json";
