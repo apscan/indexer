e8a8e028cf6d529ee486a4936056db1e2e720140
Zach Denton <z@chdenton.com>
2022-08-03 18:25:54 -0400

    Enable code splitting.

--- ecosystem/platform/server/app/javascript/controllers/nft_controller.ts
+++ ecosystem/platform/server/app/javascript/controllers/nft_controller.ts
@@ -1,6 +1,6 @@
 import { Controller } from "./controller";
 import type { FrameElement } from "@hotwired/turbo/dist/types/elements";
-import {AptosAccount, AptosClient, FaucetClient, Types} from "aptos";
+import type {AptosAccount, AptosClient, FaucetClient, Types} from "aptos";
 
 const ADDRESS = "0xb6944e1bcbededcc0be23e33ff9d6d083d2a40bf96f18ac073bf40d9c1750877";
 const MODULE = "NFT3";
@@ -14,22 +14,6 @@ async function signAndSubmit(client: AptosClient, payload: Types.TransactionPayl
   if ('version' in txn) return txn;
   throw new Error(`transaction ${txn.hash} is not on chain`);
 }
-async function createFundAccount(faucet: FaucetClient) {
-  const account = new AptosAccount();
-  await faucet.fundAccount(account.address(), 10_000);
-  return account;
-}
-
-async function createSubmitMint(client: AptosClient, account: AptosAccount) {
-  const payload = { //{ function: string; arguments: string[]; type: string; type_arguments: any[] }
-    type: "script_function_payload",
-    function: `${ADDRESS}::${MODULE}::mint`,
-    type_arguments: [],
-    arguments: []
-  };
-  return await signAndSubmit(client, payload, account);
-}
-
 // Connects to data-controller="nft"
 export default class extends Controller<FrameElement> {
   static targets = ['form', 'explorerUrl'];
@@ -45,18 +29,37 @@ export default class extends Controller<FrameElement> {
   }
 
   async mint() {
+    const aptos = await import("aptos");
+
     const NODE_URL = "https://fullnode.nft-nyc.aptoslabs.com";
     const FAUCET_URL = "https://faucet.nft-nyc.aptoslabs.com";
 
-    const client = new AptosClient(NODE_URL);
-    const faucet = new FaucetClient(NODE_URL, FAUCET_URL);
+    const client = new aptos.AptosClient(NODE_URL);
+    const faucet = new aptos.FaucetClient(NODE_URL, FAUCET_URL);
 
-    const account = await createFundAccount(faucet);
-    const txn = await createSubmitMint(client, account);
+    const account = new aptos.AptosAccount();
+    await this.fundAccount(account, faucet);
+
+    const txn = await this.createSubmitMint(client, account);
     const explorerUrl = `https://explorer.devnet.aptos.dev/txn/${txn.version}?network=nft-nyc`
 
     // Update the explorer_url.
     this.explorerUrlTarget.value = explorerUrl;
     this.formTarget.requestSubmit();
+  }
+
+  async fundAccount(account: AptosAccount, faucet: FaucetClient) {
+    await faucet.fundAccount(account.address(), 10_000);
+    return account;
+  }
+
+  async createSubmitMint(client: AptosClient, account: AptosAccount) {
+    const payload = { //{ function: string; arguments: string[]; type: string; type_arguments: any[] }
+      type: "script_function_payload",
+      function: `${ADDRESS}::${MODULE}::mint`,
+      type_arguments: [],
+      arguments: []
+    };
+    return await signAndSubmit(client, payload, account);
   }
 }
