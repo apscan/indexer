1a1f9033b31c7887b177553a29b9f867dea39140
crypto_jedi <ybn1992@gmail;.com>
2022-08-08 23:46:37 +0800

    latest_write_set_changes

---
+++ ecosystem/indexer/migrations/2022-08-08-153407_account_resource/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+CREATE TABLE latset_write_set_changes (
+    version          BIGINT       NOT NULL,
+    hash             VARCHAR(255) NOT NULL,
+    address          VARCHAR(255) NOT NULL,
+    type             TEXT         NOT NULL,
+);
