e22c7727fbff14710c7274ee02169d573033e9e1
crypto_jedi <ybn1992@gmail;.com>
2022-08-08 23:11:49 +0800

    remove sql

--- ecosystem/indexer/migrations/2022-08-08-141508_resource_view/up.sql
+++ ecosystem/indexer/migrations/2022-08-08-141508_resource_view/up.sql
@@ -1,7 +1,1 @@
 -- Your SQL goes here
-select from write_set_changes as r
-inner join
-(select address, hash, max(version) as version 
-from write_set_changes 
-where address='0xc435326568cf2f8ceaf49fc58bd6d696af9266829a8aa90c8236e9b127904f8' group by (address,hash)) as r
-on r.;
