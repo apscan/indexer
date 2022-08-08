eb4521b809fc9342421107ed03b6c0ede3fb6d15
crypto_jedi <ybn1992@gmail;.com>
2022-08-08 23:58:38 +0800

    add table

--- ecosystem/indexer/migrations/2022-08-08-153407_account_resource/up.sql
+++ ecosystem/indexer/migrations/2022-08-08-153407_account_resource/up.sql
@@ -3,5 +3,7 @@ CREATE TABLE latset_write_set_changes (
     version          BIGINT       NOT NULL,
     hash             VARCHAR(255) NOT NULL,
     address          VARCHAR(255) NOT NULL,
-    type             TEXT         NOT NULL,
+    type             TEXT         NOT NULL
+    
+    PRIMARY KEY (address, hash),
 );
